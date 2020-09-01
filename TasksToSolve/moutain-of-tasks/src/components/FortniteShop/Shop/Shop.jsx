6e1d878a6f4f8f20fd4521b66f5cb8f318d8a2f3
Oleksandr Radosh <radosanya@gmail.com>
2020-09-02 02:34:40 +0300

    Fortnite API

--- TasksToSolve/moutain-of-tasks/src/components/FortniteShop/Shop/Shop.jsx
+++ TasksToSolve/moutain-of-tasks/src/components/FortniteShop/Shop/Shop.jsx
@@ -1,14 +1,19 @@
 import React, {useState, useEffect} from "react";
 import * as axios from "axios";
 
-const APP_KEY = '3254402867981562'
+const APP_KEY = '802247c3-1f387996-bb45393a-0e5f9735'
 const Shop = () => {
 
     useEffect(() => {
         fetchItems();
     })
-    const fetchItems = async () => {
+    const fetchHeroes = async () => {
         const data = await fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json`);
+        const items = await data.json();
+        console.log(items)
+    }
+    const fetchItems = async () => {
+        const data = await fetch(`https://fortniteapi.io/v1/items/upcoming?lang=en`, {headers: {Authorization: APP_KEY}});
         const items = await data.json();
         console.log(items)
     }
